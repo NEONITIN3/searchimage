d5d47c162765c97a05200415f3832efeaf2aac8c
NEO NITIN <69511265+NEONITIN3@users.noreply.github.com>
2021-08-30 18:59:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var a=document.getElementById("text")
+function searchimages()
+{
+  window.open("https://www.google.com/search?q=travelling+&tbm=isch&ved=2ahUKEwj557i749jyAhWJynMBHT64CnoQ2-cCegQIABAA&oq=travelling+&gs_lcp=CgNpbWcQAzIHCCMQ7wMQJzIHCCMQ7wMQJzIICAAQgAQQsQMyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgcIABCxAxBDOgQIABBDOgsIABCABBCxAxCDAVCGyRxY9docYIjiHGgAcAB4AYABzwOIAc0TkgEJMC4zLjcuMC4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=DdMsYbmtNImVz7sPvvCq0Ac&bih=582&biw=711&hl=en")
+}
+
+function scrollWin() 
+{
+  window.scrollBy(0, 800);
+}
